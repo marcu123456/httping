4b150e316b99aafedbe2c9b1be720e81149fe4b6
marcu123456 <72369960+marcu123456@users.noreply.github.com>
2021-06-18 09:05:34 +1000

    fixed a bug in the install script
    Stopped it from installing the libraries to /urs/local/bin where they wouldn't be usable.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mv httpinglib/ /usr/local/bin
+mv httpinglib/ /usr/local/lib/
 echo "Installed libraries"
 touch httping
 echo python3 /usr/local/lib/httpinglib/main.py >> httping
 echo "Created excecutable"
 chmod +x httping
-mv httping /usr/local/bin
+mv httping /usr/local/bin/
 echo "Installed excecutable"
